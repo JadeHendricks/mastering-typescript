70ff1096850a255523ec743041dd042710e74e8d
Jade <hendricksjade@gmail.com>
2024-04-25 16:27:57 +0200

    add(tuple): adding in tuples in typescript + notes.

---
+++ 5_TuplesAndEnums/tuplesandenums.ts
@@ -0,0 +1,7 @@
+//Introduction Tuples
+const test: string[] = ["asasaas", "aasdadda", "121313e"]; //Normal array of strings
+//Tuples
+const colour: [number, number, number] = [255, 0, 255];
+
+type HttpResponse = [number, string];
+const goodResponse: HttpResponse = [200, "OK"];
